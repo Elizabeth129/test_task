9386573cca27c8bcbce75ebc894eed4141ec3c56
Yelyzaveta Ivanytska <liza2209129@gmail.com>
2025-04-12 16:27:02 +0300

    Try setup cloud

--- main.py
+++ main.py
@@ -44,3 +44,8 @@ def search(query: str, k: int):
     
     answer = llm.search_notes(docs, query)
     return {"answer": answer}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
